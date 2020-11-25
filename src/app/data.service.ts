4cc600ce7cc13f34133879df98ef6690420157b8
Alvin <>
2020-11-25 13:09:13 +0400

    getting the full HTTP Response with Angular HttpClient

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,14 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { throwError } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { retry, catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  private REST_API_SERVER = 'http://localhost:3000';
+  private REST_API_SERVER = 'http://localhost:3000/products';
+
+  public first = '';
+  public prev = '';
+  public next = '';
+  public last = '';
+
+  // tslint:disable-next-line:typedef
+  parseLinkHeader(header: string) {
+    if (header.length == 0) {
+      return ;
+    }
+
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach( p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+
+    });
+
+    this.first  = links["first"];
+    this.last   = links["last"];
+    this.prev   = links["prev"];
+    this.next   = links["next"];
+  }
 
   constructor(private httpClient: HttpClient) { }
 
@@ -17,7 +44,7 @@ export class DataService {
     let errorMesage = 'Unknown Error!';
     if (error.error instanceof ErrorEvent) {
       // Client Side Errors
-      errorMesage =`Error: ${error.error.message}`;
+      errorMesage = `Error: ${error.error.message}`;
     } else {
       // Server Side Errors
       errorMesage = `Error Code: ${error.status}\n Message: ${error.message}`;
@@ -28,8 +55,20 @@ export class DataService {
 
   // tslint:disable-next-line:typedef
   public sendGetRequest(){
-    // Add safe, URL encoded_page parameter
-    const options = { params: new HttpParams({fromString: '_page=1&_limit=20'}) };
-    return this.httpClient.get(this.REST_API_SERVER).pipe(retry(3), catchError(this.handleError));
+    // Add safe, URL encoded _page and _limit parameters
+
+    // tslint:disable-next-line:max-line-length
+    return this.httpClient.get(this.REST_API_SERVER, {  params: new HttpParams({fromString: '_page=1&_limit=5'}), observe: 'response'}).pipe(retry(3), catchError(this.handleError), tap(res => {
+      console.log(res.headers.get('Link'));
+      this.parseLinkHeader(res.headers.get('Link'));
+    }));
+  }
+
+  public sendGetRequestToUrl(url: string){
+    return this.httpClient.get(url, { observe: "response"}).pipe(retry(3), catchError(this.handleError), tap(res => {
+      console.log(res.headers.get('Link'));
+      this.parseLinkHeader(res.headers.get('Link'));
+
+    }));
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -10,19 +11,57 @@ import { Subject } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
-  products:any = [];
+  public firstPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.first).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
+  public previousPage() {
+
+    if (this.dataService.prev !== undefined && this.dataService.prev !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.prev).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      })
+    }
+
+  }
+  public nextPage() {
+    if (this.dataService.next !== undefined && this.dataService.next !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.next).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      })
+    }
+  }
+  public lastPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.last).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
+
+  products: any = [];
 
   destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private dataService: DataService) { }
 
+  // tslint:disable-next-line:typedef
   ngOnInit() {
-    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((data) => {
-      console.log(data);
-      this.products = data;
+
+    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
     });
   }
 
+  // tslint:disable-next-line:typedef
   ngDestroy() {
     this.destroy$.next(true);
     // Unsubscribe from the subject
