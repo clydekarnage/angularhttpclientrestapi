b3e8ed7a17f240c2393d2a7f2725a38a1bbfc8c1
Alvin <>
2020-11-25 11:46:17 +0400

    unsubscribing from HttpClient Observables with RxJS takeUntil()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +12,21 @@ export class HomeComponent implements OnInit {
 
   products:any = [];
 
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.setGetRequest().subscribe((data) => {
+    this.dataService.setGetRequest().pipe(takeUntil(this.destroy$)).subscribe((data) => {
       console.log(data);
       this.products = data;
     });
+  }
+
+  ngDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
   }
 
 }
